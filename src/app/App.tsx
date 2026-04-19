d0e871580c3bc3f899baaf06ca4718c3f9da5594
Abhishek <abhishek12439525@gmail.com>
2026-04-20 02:24:07 +0530

    feat: hydrate app with workflow persistence hook

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import { ReactFlowProvider } from '@xyflow/react';
 import { AppShell } from '@/components/layout/AppShell';
 import { useWorkflowInitialization } from '@/features/workflow/hooks/useWorkflowInitialization';
+import { useWorkflowPersistence } from '@/features/workflow/hooks/useWorkflowPersistence';
 
 export default function App() {
   useWorkflowInitialization();
+  useWorkflowPersistence();
 
   return (
     <ReactFlowProvider>
